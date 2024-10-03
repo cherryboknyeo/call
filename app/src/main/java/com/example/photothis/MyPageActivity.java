9fa155e44b5e17abeee70b57c7032e8c4d992de1
[GitHub <[GitHub>
2024-10-03 20:53:35 +0900

    1003 마이페이지 연결 오후 8시 53분

--- app/src/main/java/com/example/photothis/MyPageActivity.java
+++ app/src/main/java/com/example/photothis/MyPageActivity.java
@@ -14,11 +14,19 @@ public class MyPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
 
+        //버튼 하단 네비
         Button calendarButton = findViewById(R.id.calendarButton);
         Button todoListButton = findViewById(R.id.todoListButton);
-        Button statisticsButton = findViewById(R.id.statisticsButton);
-        Button diarySummaryButton = findViewById(R.id.diarySummaryButton);  // 요약 버튼
 
+        //버튼
+        Button setReminderButton = findViewById(R.id.setReminderButton); // 알림 설정 버튼
+        Button statisticsButton = findViewById(R.id.statisticsButton); // 통계 버튼
+        Button diarySummaryButton = findViewById(R.id.diarySummaryButton);  // 요약 버튼
+        Button contactAdminButton = findViewById(R.id.contactAdminButton); // 문의하기 버튼
+        Button disconnectNaverButton = findViewById(R.id.disconnectNaverButton); // 네이버 연동 해제 버튼
+
+        // 하단 네비게이션 연결
+        // 1. 메인 페이지 (캘린더)
         calendarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,6 +35,7 @@ public class MyPageActivity extends AppCompatActivity {
             }
         });
 
+        // 2. 투두리스트
         todoListButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,6 +44,17 @@ public class MyPageActivity extends AppCompatActivity {
             }
         });
 
+        // 버튼 연결
+        // 1. 알림 설정
+        setReminderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyPageActivity.this, SetReminderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 2. 통계
         statisticsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,10 +63,29 @@ public class MyPageActivity extends AppCompatActivity {
             }
         });
 
+        // 3. 요약
         diarySummaryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MyPageActivity.this, DiarySummaryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 4. 문의하기
+        contactAdminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyPageActivity.this, ContactAdminActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 5. 네이버 연동 해제
+        disconnectNaverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyPageActivity.this, DisconnectNaverActivity.class);
                 startActivity(intent);
             }
         });
